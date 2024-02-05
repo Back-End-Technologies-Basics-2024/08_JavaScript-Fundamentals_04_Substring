265e8c753a84a58b51035f34e1088502c7bb1fe2
DesiZaharieva <135128496+DesiZaharieva@users.noreply.github.com>
2024-02-05 14:26:18 +0200

    Add files via upload

---
+++ 4_Substring.js
@@ -0,0 +1,13 @@
+function solve(string, startIndex, countElements) {
+    'use strict';
+    
+    let result = string.substring(startIndex, startIndex + countElements);
+
+
+    console.log(result);
+
+
+    }
+
+
+    solve('SkipWord', 4, 7)
